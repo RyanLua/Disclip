9a090be912f84cc5d95817c7f2aea1fc740a5205
Ryan Luu <ryan.luu@ryanluu.dev>
2025-06-22 23:03:40 -0400

    Add context
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- commands/utility/clip.js
+++ commands/utility/clip.js
@@ -1,9 +1,14 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, InteractionContextType } = require('discord.js');
 
 module.exports = {
 	data: new ContextMenuCommandBuilder()
 		.setName('Clip Message')
-		.setType(ApplicationCommandType.Message),
+		.setType(ApplicationCommandType.Message)
+		.setContexts([
+			InteractionContextType.BotDM,
+			InteractionContextType.PrivateChannel,
+			InteractionContextType.Guild,
+		]),
 	async execute(interaction) {
 		await console.log(interaction);
 	},
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,9 +1,14 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, InteractionContextType } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.setContexts([
+			InteractionContextType.BotDM,
+			InteractionContextType.PrivateChannel,
+			InteractionContextType.Guild,
+		]),
 	async execute(interaction) {
 		await interaction.reply({
 			content: 'Pong!',
