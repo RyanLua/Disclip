bb59af175818bc43f9d394f1b329a57e49f1dc82
Ryan Luu <ryan.luu@ryanluu.dev>
2025-07-10 21:51:25 -0700

    Add types
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/clip.js
+++ src/clip.js
@@ -4,6 +4,11 @@
 
 import puppeteer from '@cloudflare/puppeteer';
 
+/**
+ * Generates HTML content for a Discord message.
+ * @param {import('discord-api-types/v10').APIMessage} message - The Discord message object.
+ * @returns {string} - The generated HTML content.
+ */
 function generateHtml(message) {
 	const author = message.author;
 	const username = author.username;
@@ -142,6 +147,12 @@ function generateHtml(message) {
 	return htmlTemplate;
 }
 
+/**
+ * Generate a message screenshot from a Discord message.
+ * @param {import('discord-api-types/v10').APIMessage} message - The Discord message object.
+ * @param {*} env - The environment variables.
+ * @returns {Promise<Buffer>} - The screenshot image buffer.
+ */
 async function generateMessageScreenshot(message, env) {
 	let browser;
 	try {
@@ -164,6 +175,11 @@ async function generateMessageScreenshot(message, env) {
 	}
 }
 
+/**
+ * Generate a message clip from a Discord interaction.
+ * @param {import('discord-api-types/v10').APIInteraction} interaction - The Discord interaction object.
+ * @param {*} env - The environment variables.
+ */
 export async function generateMessageClip(interaction, env) {
 	const targetId = interaction.data.target_id;
 	const targetMessage = interaction.data.resolved.messages[targetId];
