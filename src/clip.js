f354520e26f80b0d3232582ac54c01e5debbc788
Ryan Luu <ryan.luu@ryanluu.dev>
2025-07-27 00:58:56 +0000

    Add basic message parsing
    Signed-off-by: GitHub <noreply@github.com>

--- src/clip.js
+++ src/clip.js
@@ -13,7 +13,7 @@ import style from '../public/style.css';
  * @param {*} env - The environment variables.
  * @returns {Promise<Buffer>} - The screenshot image buffer.
  */
-async function generateMessageScreenshot(_message, env) {
+async function generateMessageScreenshot(message, env) {
 	// Pick random session from open sessions
 	let sessionId = await getRandomSession(env.BROWSER);
 	let browser;
@@ -37,10 +37,33 @@ async function generateMessageScreenshot(_message, env) {
 
 	sessionId = browser.sessionId(); // get current session id
 
+	// Extract message data
+	const author = message.author;
+	const username = author.username;
+	const avatarUrl = author.avatar
+		? `https://cdn.discordapp.com/avatars/${author.id}/${author.avatar}.png`
+		: null;
+	const serverTag = author.clan?.tag || '';
+	const serverTagBadge = author.clan
+		? `https://cdn.discordapp.com/guild-tag-badges/${author.clan.identity_guild_id}/${author.clan.badge}.png`
+		: '';
+	const messageContent = message.content;
+
 	// Generate the screenshot
 	const page = await browser.newPage();
 	await page.setContent(index);
 	await page.addStyleTag({ content: style });
+
+	await page.evaluate(
+		(avatarUrl, username, messageContent) => {
+			document.querySelector('.avatar').setAttribute('src', avatarUrl);
+			document.querySelector('.username').textContent = username;
+			document.querySelector('.message').textContent = messageContent;
+		},
+		avatarUrl,
+		username,
+		messageContent,
+	);
 
 	const cardElement = await page.$('.card');
 	const cardBoundingBox = await cardElement.boundingBox();
